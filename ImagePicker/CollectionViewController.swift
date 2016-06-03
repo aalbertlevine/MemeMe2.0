9618720d54a7c931ca7e71eb45b568fd2f2132ec
Austin Levine <austinl@uber.com>
2016-06-02 19:38:16 -0700

    updates

--- ImagePicker/CollectionViewController.swift
+++ ImagePicker/CollectionViewController.swift
@@ -21,11 +21,12 @@ class CollectionViewController: UICollectionViewController {
         super.viewDidLoad()
         
         let space: CGFloat = 3.0
-        let dimension = (self.view.frame.size.width - (2 * space)) / 3.0
+        let width = (self.view.frame.size.width - (2 * space)) / 3.0
+        let height = (self.view.frame.size.height - (2 * space)) / 3.0
         
         flowLayout.minimumInteritemSpacing = space
         flowLayout.minimumLineSpacing = space
-        flowLayout.itemSize = CGSizeMake(dimension, dimension)
+        flowLayout.itemSize = CGSizeMake(width, height)
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -41,7 +42,7 @@ class CollectionViewController: UICollectionViewController {
     override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
         
         let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MemeCollectionViewCell", forIndexPath: indexPath) as! MemeCollectionViewCell
-        let meme = self.memes[indexPath.row]
+        let meme = self.memes[indexPath.item]
         
         // Set the image
         cell.memeImageView.image = meme.memedImage
